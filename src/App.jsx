d37ecce7fffd96285fc130145e3e6ddc75fb16e0
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-03-11 22:51:39 +0600

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+import Countries from "./Com/Countries";
+// import Country from './Com/Counrties/Country/Country'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1> React World Tour</h1>
+      {/* <Country></Country> */}
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
